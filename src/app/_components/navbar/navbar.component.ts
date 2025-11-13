d17b0e8b62ecbe07aa2eae993e17d84a339eec1e
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-11-13 23:45:14 +0000

    FEATURE: componente botao primario e navbar

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('NavbarComponent initialized');
+  }
+
+
+
+}
