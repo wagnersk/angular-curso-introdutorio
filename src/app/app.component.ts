78f1d80e7cb090fddd9b28bafd6e84e8ee37b524
Wagner <65842719+wagnersk@users.noreply.github.com>
2025-11-15 12:14:20 +0000

    FEATURE: Page aviso de lista de certificados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { CommonModule } from '@angular/common';
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUIComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUIComponent, CertificadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gerador-certificado';
+  exibeNabar:boolean = true;
 }
